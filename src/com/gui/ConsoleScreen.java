a10efe592fd0b81c55582c6389e869099a1c68aa
Michael Razgoner <mrazgoner@gmail.com>
2018-02-11 17:37:42 +0200

    first update

---
+++ src/com/gui/ConsoleScreen.java
@@ -0,0 +1,45 @@
+package com.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+import javax.swing.JScrollPane;
+
+/**
+ * 
+ * @author Owner
+ *
+ */
+
+@SuppressWarnings("serial")
+public class ConsoleScreen extends JFrame{
+
+	private JTextArea textArea;
+
+	public ConsoleScreen() {
+		getContentPane().setLayout(null);
+
+		//740X490
+		setSize(800, 650);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		//setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(20, 30, 750, 540);
+		getContentPane().add(scrollPane);
+
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setEditable(false);
+
+	}
+
+	public void append(String str)
+	{
+		textArea.append(str);
+		textArea.setCaretPosition(textArea.getDocument().getLength());
+
+	}
+
+}
---
+++ src/com/main/Logger.java
@@ -0,0 +1,13 @@
+package com.main;
+
+
+public class Logger {
+	
+
+	public void append(String str) {
+		//TODO implement
+	}
+	
+	
+
+}

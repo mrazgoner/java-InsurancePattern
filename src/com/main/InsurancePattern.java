128a2dfc2460ec85ba35235efe330461b99f0d92
Michael Razgoner <mrazgoner@gmail.com>
2018-02-15 20:14:16 +0200

    jdbc

--- src/com/main/InsurancePattern.java
+++ src/com/main/InsurancePattern.java
@@ -3,24 +3,27 @@ package com.main;
 
 import java.util.Date;
 
+import com.database.DatabaseConnnector;
 import com.gui.ConsoleScreen;
 import com.settings.SettingsDataAccess;
 
 public class InsurancePattern {
-
+	
 	static ConsoleScreen console;
 	static Logger logger;
 	static OutStreamPrinter outStream;
 	static SettingsDataAccess settings;
 
 	public static void main(String[] args) {
-
+		
 		setupOutput();
 
 		console.setVisible(true);
 		settings = new SettingsDataAccess("settings.xml");
 		logger = new Logger(settings.getLoggerPath());
 		outStream.addObserver(logger);
+		
+		//DatabaseConnnector.initialize(Host, BaseName, User, Password);
 		
 		System.out.println(new Date());
 		

cf5886873769501aa0340a25e083cd5fb70a118a
Itai <Itai@Itai-PC>
2018-02-16 13:00:13 +0200

    .

--- src/com/gui/ClientUI.java
+++ src/com/gui/ClientUI.java
@@ -10,7 +10,9 @@ import com.entity.ScreensInfo;
 
 import com.control.LoggerController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Screen;
@@ -20,7 +22,7 @@ public class ClientUI extends Application {
 
 
 	public static void main(String args[]) {
-			Application.launch(ClientUI.class, args);
+		Application.launch(args);
 			
 	}
 
@@ -34,20 +36,30 @@ public class ClientUI extends Application {
 		     ScreenController screenController = new ScreenController();
 		     screenController.replaceSceneContent(ScreensInfo.HOMEPAGE_SCREEN,ScreensInfo.HOMEPAGE_TITLE);
 		     primaryStage.setResizable(false);
-		     primaryStage.show();
+		     
 			 primaryStage.setX(primaryScreenBounds.getMaxX()/2.0 - primaryStage.getWidth()/2.0);
 			 primaryStage.setY(primaryScreenBounds.getMaxY()/2.0 - primaryStage.getHeight()/2.0);
 			 
-			 /*
-			 ScreenController screenController2 = new ScreenController();
-			 LoggerController logger = new LoggerController();
-			 screenController2.replaceSceneContent(ScreensInfo.LOGGER_SCREEN,ScreensInfo.LOGGER_TITLE);
-			 Stage secondStage  = screenController2.getStage();
-			 ScreenController.setStage(secondStage);
-			 secondStage.show();
-			 secondStage.setX(primaryScreenBounds.getMaxX()/2.0 - secondStage.getWidth()/2.0);
-			 secondStage.setY(primaryScreenBounds.getMaxY()/2.0 - secondStage.getHeight()/2.0);
-			 */
+			 
+		    FXMLLoader loader = new FXMLLoader(getClass().getResource(ScreensInfo.LOGGER_SCREEN));
+	        Scene newScene;
+	        try {
+	            newScene = new Scene(loader.load());
+	        } catch (IOException ex) {
+	            // TODO: handle error
+	            return;
+	        }
+	        Stage inputStage = new Stage();
+	        inputStage.initOwner(primaryStage);
+	        inputStage.setScene(newScene);
+	        inputStage.show();
+	        inputStage.setResizable(false);
+	        //inputStage.setOnCloseRequest(e->e.consume());
+
+	        primaryStage.show();
+			 //screenController.replaceSceneContent(ScreensInfo.LOGGER_SCREEN,ScreensInfo.LOGGER_TITLE);
+
+			 
 			 
 		     } catch (Exception e) {
 				Alert alert = new Alert(AlertType.INFORMATION);
@@ -58,6 +70,8 @@ public class ClientUI extends Application {
 				return;
 		        }
 	}
+	
+	
 
 	
 }
